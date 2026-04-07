74b457e2eda4a88c7ab10be1fd771282bededae9
niancpu <1756583517@qq.com>
2026-04-07 11:49:57 +0800

    正在做add的资源库实现

--- ProjectsPractice/JavaBookManage/src/main/java/dao/impl/AdminOperImpl.java
+++ ProjectsPractice/JavaBookManage/src/main/java/dao/impl/AdminOperImpl.java
@@ -5,12 +5,19 @@ import main.java.dao.AdminOperation;
 import main.java.dao.DatabaseOperation;
 import main.java.dao.UserOperation;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class AdminOperImpl implements AdminOperation {
-    UserOperation Uoper=new UserOperImpl();
-    DatabaseOperation oper=new DbOperFileImpl();
-    BookEntity book=new BookEntity();
+    private final UserOperation Uoper=new UserOperImpl();
+    private final DatabaseOperation oper=new DbOperFileImpl();
+    private final Path dbpath = Paths.get("db.dat");
+
     @Override
     public boolean changeName (String originalName,String name){
+        BookEntity book;
         if((book=Uoper.find(originalName))!=null){
             book.setBookName(name);
             oper.updata(book);
@@ -22,6 +29,7 @@ public class AdminOperImpl implements AdminOperation {
     }
     @Override
     public boolean setPrice(String name,float price){
+        BookEntity book;
         if((book=Uoper.find(name))!=null){
             book.setPrice(price);
             oper.updata(book);
@@ -29,6 +37,19 @@ public class AdminOperImpl implements AdminOperation {
         }
         else{
             return false;
+        }
+    }
+    @Override
+    public boolean addBook(String name,float price) throws IOException {
+        try {
+            long dbsize= Files.size(dbpath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        BookEntity book=new BookEntity();
+        book.setBookName(name);
+        book.setPrice(price);
+        int id=
         }
     }
 }
