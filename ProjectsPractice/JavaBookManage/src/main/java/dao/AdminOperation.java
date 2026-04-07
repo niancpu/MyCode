4f1ae1df1f157e8d97d8cc4963ae555663c387c7
niancpu <1756583517@qq.com>
2026-04-07 11:49:50 +0800

    正在做add的资源库实现

--- ProjectsPractice/JavaBookManage/src/main/java/dao/AdminOperation.java
+++ ProjectsPractice/JavaBookManage/src/main/java/dao/AdminOperation.java
@@ -1,6 +1,9 @@
 package main.java.dao;
 
+import java.io.IOException;
+
 public interface AdminOperation {
     public boolean setPrice(String Name,float price);
     public boolean changeName(String originalName,String name);
+    public boolean addBook(String name,float price) throws IOException;
 }
